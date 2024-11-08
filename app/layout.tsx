e3fa0494b4bfebb832d7708fdcd25b384b05d462
王新 <wx@EdMBP.local>
2024-11-08 13:40:06 +0800

    Completed the Course

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,24 @@
-import "@/app/ui/global.css";
-import { inter } from "@/app/ui/fonts";
+import '@/app/ui/global.css';
+import { inter } from '@/app/ui/fonts';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+	title: {
+		template: '%s | Acme Dashboard',
+		default: 'Acme Dashboard',
+	},
+	description: 'The official Next.js Course Dashboard, built with App Router.',
+	metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
+};
+
 export default function RootLayout({
-  children,
+	children,
 }: {
-  children: React.ReactNode;
+	children: React.ReactNode;
 }) {
-  return (
-    <html lang="en">
-      <body className={`${inter.className}`}>{children}</body>
-    </html>
-  );
+	return (
+		<html lang="en">
+			<body className={`${inter.className}`}>{children}</body>
+		</html>
+	);
 }
